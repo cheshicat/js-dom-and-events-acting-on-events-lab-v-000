9dec098a41440537cedf35e9c4fa01e12ba1bff4
Allison Howard <alliemhow@gmail.com>
2019-09-05 06:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function resetInput() {
 function clearEmployeeListOnLinkClick() {
   let link = document.querySelector('a');
   let ul = document.querySelector('ul');
+  
   link.addEventListener('click', function(event) {
   });
 }
